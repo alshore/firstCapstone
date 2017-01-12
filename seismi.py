b6f0ed6ec984a5c2c0fd9bfa1ac55f657cc404ec
alshore <andrewlshore@gmail.com>
2017-01-12 16:18:00 -0600

    Add files via upload

--- seismi.py
+++ seismi.py
@@ -1,7 +1,6 @@
 import urllib
 import json
-from collections import namedtuple
-
+# generate url
 url = "http://www.seismi.org/api/eqs/"
 url += raw_input("Enter the year to check: ")
 url += "/"
@@ -9,10 +8,11 @@ url += raw_input("Enter the month (03 for March): ")
 url += "?"
 url += "min_magnitude="
 url += raw_input("Enter minimum magnitude: ")
-
+# use urllib to open url
 result = urllib.urlopen(url)
+# read json data from url
 data = json.loads(result.read())
-
+# print json info as dictionary
 print data
-
+# just so the results appear and stay (not disappear)
 xit = raw_input("Do you want to continue? ")
